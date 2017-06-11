accb7328af617e5ed5f08a0d8b9c2a2410615214
Sergi <sergibernaus@yahoo.es>
2017-06-11 15:54:30 +0200

    Arrays

--- spec/4.Arrays.js
+++ spec/4.Arrays.js
@@ -1,318 +1,328 @@
 describe("Arrays", () => {
 
-	describe("constants follows JSON rules", () => {
+    describe("constants follows JSON rules", () => {
 
-		it("[1,2,3], [42,\"sense\",[\"of\", \"life\"]], []", () => {
-			expect("solve me").toEqual(jasmine.any(Array));
-			expect("solve me").toEqual(jasmine.any(Array));
-			expect("solve me").toEqual(jasmine.any(Array));
-		});
+        it("[1,2,3], [42,\"sense\",[\"of\", \"life\"]], []", () => {
+            expect([1, 2, 3]).toEqual(jasmine.any(Array));
+            expect([42, "sense", ["of", "life"]]).toEqual(jasmine.any(Array));
+            expect([]).toEqual(jasmine.any(Array));
+        });
 
-	});
+    });
 
-	describe("get set", () => {
+    describe("get set", () => {
 
-		let array;
-		beforeEach(() => { array = [1,2,3]; });
+        let array;
+        beforeEach(() => {
+            array = [1, 2, 3];
+        });
 
-		it("you can use [index] to read any position", () => {
-			expect("solve me").toBe(array[1]);
-		});
+        it("you can use [index] to read any position", () => {
+            expect(2).toBe(array[1]);
+        });
 
-		it("you can use [index] to set any position", () => {
-			array[1] = 4;
-			expect("solve me").toEqual(array);
-		});
+        it("you can use [index] to set any position", () => {
+            array[1] = 4;
+            expect([1, 4, 3]).toEqual(array);
+        });
 
-		it("you can mix types", () => {
-			array[1] = "of";
-			expect("solve me").toEqual(array);
-		});
+        it("you can mix types", () => {
+            array[1] = "of";
+            expect([1, "of", 3]).toEqual(array);
+        });
 
-		it("you get undefined if you read an unset number", () => {
-			expect("solve me").toBe(array[5]);
-			expect("solve me").toBe(array[-1]);
-		});
+        it("you get undefined if you read an unset number", () => {
+            expect(undefined).toBe(array[5]);
+            expect(undefined).toBe(array[-1]);
+        });
 
-		it("you can set any position (grows dinamically)", () => {
-			array[4] = 5;
-			expect("solve me").toBe(array[4]);
-		});
+        it("you can set any position (grows dinamically)", () => {
+            array[4] = 5;
+            expect(5).toBe(array[4]);
+        });
 
-		it("all positions between the setted and last become undefined", () => {
-			array[4] = 5;
-			expect("solve me").toEqual(array);
-		});
+        it("all positions between the setted and last become undefined", () => {
+            array[4] = 5;
+            expect([1, 2, 3, undefined, 5]).toEqual(array);
+        });
 
-	});
+    });
 
-	describe("length", () => {
+    describe("length", () => {
 
-		let array;
-		beforeEach(() => { array = [1,2,3]; });
+        let array;
+        beforeEach(() => {
+            array = [1, 2, 3];
+        });
 
-		it("length returns the actual length of the array", () => {
-			expect("solve me").toBe(array.length);
-		});
+        it("length returns the actual length of the array", () => {
+            expect(3).toBe(array.length);
+        });
 
-		it("length can be set to shrink the array", () => {
-			array.length = 2;
-			expect("solve me").toEqual(array);
-		});
+        it("length can be set to shrink the array", () => {
+            array.length = 2;
+            expect([1, 2]).toEqual(array);
+        });
 
-		it("length can be set to enlarge the array filled with undefined", () => {
-			array.length = 5;
-			expect("solve me").toEqual(array);
-		});
+        it("length can be set to enlarge the array filled with undefined", () => {
+            array.length = 5;
+            expect([1, 2, 3, undefined, undefined]).toEqual(array);
+        });
 
-		it("length grows when more elements are added", () => {
-			array[4] = 5;
-			expect("solve me").toEqual(array.length);
-		});
+        it("length grows when more elements are added", () => {
+            array[4] = 5;
+            expect(5).toEqual(array.length);
+        });
 
-	});
+    });
 
-	describe("can mutate", () => {
+    describe("can mutate", () => {
 
-		let array;
-		beforeEach(() => { array = [1,2,3]; });
+        let array;
+        beforeEach(() => {
+            array = [1, 2, 3];
+        });
 
-		it("unmutated array", () => {
-			expect("solve me").toEqual(array);
-		});
+        it("unmutated array", () => {
+            expect([1, 2, 3]).toEqual(array);
+        });
 
-		describe("can behave as a stack", () => {
+        describe("can behave as a stack", () => {
 
-			describe("from behind", () => {
+            describe("from behind", () => {
 
-				it("push adds a new element to the end", () => {
-					array.push(4);
-					expect("solve me").toEqual(array);
-				});
+                it("push adds a new element to the end", () => {
+                    array.push(4);
+                    expect([1, 2, 3, 4]).toEqual(array);
+                });
 
-				it("pop removes the last element", () => {
-					array.pop();
-					expect("solve me").toEqual(array);
-				});
+                it("pop removes the last element", () => {
+                    array.pop();
+                    expect([1, 2]).toEqual(array);
+                });
 
-				it("pop returns the value of the last element", () => {
-					expect("solve me").toBe(array.pop());
-				});
+                it("pop returns the value of the last element", () => {
+                    expect(3).toBe(array.pop());
+                });
 
-			});
+            });
 
-			describe("from head", () => {
+            describe("from head", () => {
 
-				it("unshift adds a new element to the beggining", () => {
-					array.unshift(0);
-					expect("solve me").toEqual(array);
-				});
+                it("unshift adds a new element to the beggining", () => {
+                    array.unshift(0);
+                    expect([0, 1, 2, 3]).toEqual(array);
+                });
 
-				it("pop removes the first element", () => {
-					array.shift();
-					expect("solve me").toEqual(array);
-				});
+                it("pop removes the first element", () => {
+                    array.shift();
+                    expect([2, 3]).toEqual(array);
+                });
 
-				it("pop returns the value of the first element", () => {
-					expect("solve me").toBe(array.shift());
-				});
+                it("pop returns the value of the first element", () => {
+                    expect(1).toBe(array.shift());
+                });
 
-			});
+            });
 
-		});
+        });
 
 
-		describe("can behave as a queue", () => {
+        describe("can behave as a queue", () => {
 
-			it("you can add elements in the end and get them from the front", () => {
-				array.push(4);
-				array.shift();
+            it("you can add elements in the end and get them from the front", () => {
+                array.push(4);
+                array.shift();
 
-				expect("solve me").toEqual(array);
-			});
+                expect([2, 3, 4]).toEqual(array);
+            });
 
-			it("you can add elements in the head and get them from the tail", () => {
-				array.unshift(0);
-				array.pop();
+            it("you can add elements in the head and get them from the tail", () => {
+                array.unshift(0);
+                array.pop();
 
-				expect("solve me").toEqual(array);
-			});
+                expect([0, 1, 2]).toEqual(array);
+            });
 
-		});
+        });
 
-		describe("splice", () => {
+        describe("splice", () => {
 
-			it("splice can insert numbers in any position", () => {
-				array = ["a", "c", "d"];
-				array.splice(1, 0, "b");
-				expect("solve me").toEqual(array);
-			});
+            it("splice can insert numbers in any position", () => {
+                array = ["a", "c", "d"];
+                array.splice(1, 0, "b");
+                expect(["a", "b", "c", "d"]).toEqual(array);
+            });
 
-			it("splice can remove numbers in any position", () => {
-				array = ["a", "x", "b", "c"];
-				array.splice(1, 1);
-				expect("solve me").toEqual(array);
-			});
+            it("splice can remove numbers in any position", () => {
+                array = ["a", "x", "b", "c"];
+                array.splice(1, 1);
+                expect(["a", "b", "c"]).toEqual(array);
+            });
 
-		});
+        });
 
-		describe("sort", () => {
+        describe("sort", () => {
 
-			it("sort orders array contents", () => {
-				array = ["a", "c", "b"];
-				array.sort();
+            it("sort orders array contents", () => {
+                array = ["a", "c", "b"];
+                array.sort();
 
-				expect("solve me").toEqual(array);
-			});
+                expect(["a", "b", "c"]).toEqual(array);
+            });
 
-			it("sort returns the array itself ordered", () => {
-				array = ["a", "c", "b"];
+            it("sort returns the array itself ordered", () => {
+                array = ["a", "c", "b"];
 
-				expect("solve me").toEqual(array.sort());
-			});
+                expect(["a", "b", "c"]).toEqual(array.sort());
+            });
 
-			it("sort orders strings by default, if no, it imagines strings", () => {
-				array = [1, 5, 10];
+            it("sort orders strings by default, if no, it imagines strings", () => {
+                array = [1, 5, 10];
 
-				expect("solve me").toEqual(array.sort());
-			});
+                expect([1, 10, 5]).toEqual(array.sort());
+            });
 
-			it("sort accepts a ordering function", () => {
-				array = [1, 5, 10];
+            it("sort accepts a ordering function", () => {
+                array = [1, 5, 10];
 
-				expect("solve me").toEqual(array.sort((a, b) => a - b));
-			});
+                expect([1, 5, 10]).toEqual(array.sort((a, b) => a - b));
+            });
 
-		});
+        });
 
-		describe("reverse", () => {
-	
-			it("reverses the contents of an array", () => {
-				array.reverse();
-				expect("solve me").toEqual(array);
-			});
+        describe("reverse", () => {
 
-			it("returns the reversed array itself", () => {
-				expect("solve me").toEqual(array.reverse());
-			});
+            it("reverses the contents of an array", () => {
+                array.reverse();
+                expect([3, 2, 1]).toEqual(array);
+            });
 
-		});
+            it("returns the reversed array itself", () => {
+                expect([3, 2, 1]).toEqual(array.reverse());
+            });
 
-	});
+        });
 
-	describe("non mutating operations", () => {
+    });
 
-		let array;
-		beforeEach(() => { array = [1,2,3]; });
+    describe("non mutating operations", () => {
 
-		describe("slice", () => {
-			it("slice copies an array", () => {
-				let copy = array.slice();
-				array[2] = "c";
+        let array;
+        beforeEach(() => {
+            array = [1, 2, 3];
+        });
 
-				expect("solve me").toEqual(copy);
-			});
+        describe("slice", () => {
+            it("slice copies an array", () => {
+                let copy = array.slice();
+                array[2] = "c";
 
-			it("slice(begin,end) can copy a part of an array", () => {
-				expect("solve me").toEqual(["a", "b", 1, 2, 3, "c"].slice(2, 5));
-			});
+                expect([1, 2, 3]).toEqual(copy);
+            });
 
-			it("slice(position) copies from the position to the end", () => {
-				expect("solve me").toEqual(array.slice(1));
-			});
+            it("slice(begin,end) can copy a part of an array", () => {
+                expect([1, 2, 3]).toEqual(["a", "b", 1, 2, 3, "c"].slice(2, 5));
+            });
 
-			it("slice(-position) copies from the last nth position to the end", () => {
-				expect("solve me").toEqual(array.slice(-1));
-			});
-		});
+            it("slice(position) copies from the position to the end", () => {
+                expect([2, 3]).toEqual(array.slice(1));
+            });
 
-		describe("concat", () => {
-			it("concatenates another array", () => {
-				expect("solve me").toEqual(array.concat([4,5]));
-			});
+            it("slice(-position) copies from the last nth position to the end", () => {
+                expect([3]).toEqual(array.slice(-1));
+            });
+        });
 
-			it("does not modifies the array", () => {
-				array.concat([4,5]);
-				expect("solve me").toEqual(array);
-			});
-		});
+        describe("concat", () => {
+            it("concatenates another array", () => {
+                expect([1, 2, 3, 4, 5]).toEqual(array.concat([4, 5]));
+            });
 
-		describe("iterators", () => {
+            it("does not modifies the array", () => {
+                array.concat([4, 5]);
+                expect([1, 2, 3]).toEqual(array);
+            });
+        });
 
-			it("filter selects elements from the array", () => {
-				expect("solve me").toEqual(array.filter(n => n % 2 === 1));
-			});
+        describe("iterators", () => {
 
-			it("map applies a function to each element", () => {
-				expect("solve me").toEqual(array.map(n => n * 2));
-			});
+            it("filter selects elements from the array", () => {
+                expect([1, 3]).toEqual(array.filter(n => n % 2 === 1));
+            });
 
-			it("reduce(fn, initial) applies an accumulator operation", () => {
-				expect("solve me").toEqual(array.reduce((s, n) => s + n, 0));
-			});
+            it("map applies a function to each element", () => {
+                expect([2, 4, 6]).toEqual(array.map(n => n * 2));
+            });
 
-			it("every evaluates if a condition is satisfied by all elements", () => {
-				expect("solve me").toBe(array.every(n => n >= 0));
-				expect("solve me").toBe(array.every(n => n % 2 === 0));
-				expect("solve me").toBe(array.every(n => n === -1));
-			});
+            it("reduce(fn, initial) applies an accumulator operation", () => {
+                expect(6).toEqual(array.reduce((s, n) => s + n, 0));
+            });
 
-			it("some evaluates if a condition is satisfied by any elements", () => {
-				expect("solve me").toBe(array.some(n => n >= 0));
-				expect("solve me").toBe(array.some(n => n % 2 === 0));
-				expect("solve me").toBe(array.every(n => n === -1));
-			});
+            it("every evaluates if a condition is satisfied by all elements", () => {
+                expect(true).toBe(array.every(n => n >= 0));
+                expect(false).toBe(array.every(n => n % 2 === 0));
+                expect(false).toBe(array.every(n => n === -1));
+            });
 
-			it("forEach executes a function for each element", () => {
-				let count = 0;
-				array.forEach(n => count += 1);
+            it("some evaluates if a condition is satisfied by any elements", () => {
+                expect(true).toBe(array.some(n => n >= 0));
+                expect(true).toBe(array.some(n => n % 2 === 0));
+                expect(false).toBe(array.every(n => n === -1));
+            });
 
-				expect("solve me").toBe(count);
-			});
+            it("forEach executes a function for each element", () => {
+                let count = 0;
+                array.forEach(n => count += 1);
 
-			it("none modifies the array", () => {
-				array.filter(n => n % 2 === 1);
-				array.map(n => n * 2);
-				array.reduce((s, n) => s + n, 0);
-				array.every(n => n >= 0);
-				array.some(n => n === 2);
-				array.forEach(n => n++);
+                expect(3).toBe(count);
+            });
 
-				expect("solve me").toEqual(array);
-			});
+            it("none modifies the array", () => {
+                array.filter(n => n % 2 === 1);
+                array.map(n => n * 2);
+                array.reduce((s, n) => s + n, 0);
+                array.every(n => n >= 0);
+                array.some(n => n === 2);
+                array.forEach(n => n++);
 
-		});
+                expect([1, 2, 3]).toEqual(array);
+            });
 
-		describe("finders", () => {
+        });
 
-			beforeEach(() => { array = ["a", "b", "c"]; });
+        describe("finders", () => {
 
-			it("indexOf(value) looks for the same exact value position", () => {
-				expect("solve me").toBe(array.indexOf("b"));
-			});
+            beforeEach(() => {
+                array = ["a", "b", "c"];
+            });
 
-			it("indexOf(value) returns -1 if the element does not exists", () => {
-				expect("solve me").toBe(array.indexOf("e"));
-			});
+            it("indexOf(value) looks for the same exact value position", () => {
+                expect(1).toBe(array.indexOf("b"));
+            });
 
-			it("find returns the first element that satisfies a condition", () => {
-				expect("solve me").toBe(array.find(l => l > "a"));
-			});
+            it("indexOf(value) returns -1 if the element does not exists", () => {
+                expect(-1).toBe(array.indexOf("e"));
+            });
 
-			it("find returns undefined if no element satisfies the condition", () => {
-				expect("solve me").toBe(array.find(l => l > "e"));
-			});
+            it("find returns the first element that satisfies a condition", () => {
+                expect("b").toBe(array.find(l => l > "a"));
+            });
 
-			it("findIndex returns the position of the first element that satisfies a condition", () => {
-				expect("solve me").toBe(array.findIndex(l => l > "a"));
-			});
+            it("find returns undefined if no element satisfies the condition", () => {
+                expect(undefined).toBe(array.find(l => l > "e"));
+            });
 
-			it("findIndex returns -1 if no element satisfies the condition", () => {
-				expect("solve me").toBe(array.findIndex(l => l > "e"));
-			});
+            it("findIndex returns the position of the first element that satisfies a condition", () => {
+                expect(1).toBe(array.findIndex(l => l > "a"));
+            });
 
-		});
+            it("findIndex returns -1 if no element satisfies the condition", () => {
+                expect(-1).toBe(array.findIndex(l => l > "e"));
+            });
 
-	});
+        });
+
+    });
 
 });
